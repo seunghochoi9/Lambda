b23b6b3bcbbe9c4e96420e9e92724a06c73a4528
choiseungho <jackpot97912@gmail.com>
2024-03-07 18:29:49 +0900

    test

--- user/UserRepository.java
+++ user/UserRepository.java
@@ -1,4 +1,60 @@
 package user;
 
+import java.sql.*;
+import java.util.List;
+import java.util.Map;
+
 public class UserRepository {
+
+    private static UserRepository instance;
+
+    static {
+        try {
+            instance = new UserRepository();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static UserRepository getInstance() {
+        return instance;
+    }
+
+    private Connection connection;
+
+    private UserRepository() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/erichgammadb",
+                "erichgamma",
+                "erichgammadb");
+    }
+
+
+    public String test() {
+        return "UserRepository 연결";
+    }
+
+    public List<?> findUsers() throws SQLException {
+        String sql = "select * from board";
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        ResultSet resultSet = pstmt.executeQuery();
+        if(resultSet.next()){
+            do{
+                System.out.println("-- inner ---");
+                System.out.printf("ID: %d\t Title: %s\t Content: %s\t Writer: %s\n",
+                        resultSet.getInt("id"),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                System.out.println();
+            }while(resultSet.next());
+
+        }else{
+            System.out.println("데이터가 없습니다.");
+        }
+        resultSet.close();
+        pstmt.close();
+        connection.close();
+        return null;
+    }
 }
